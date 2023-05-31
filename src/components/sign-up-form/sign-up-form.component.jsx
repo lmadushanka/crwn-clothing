8aae837caeb56a7789df1a81fdefb3359a3a61a1
lakshitha <kgalmadushanka@gmail.com>
2023-05-31 21:59:35 +0530

    completed authentication

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
-import "./sign-up-form.styles.scss";
+import { useState } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
-} from "../../util/firebase/firebase.utils";
-import FormInput from "../form-input/form-input.component";
+} from '../../util/firebase/firebase.utils';
 
-import Button from "../button/button.component";
-
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
-  displayName: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
 };
 
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
-
-  console.log(formFields);
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -30,16 +29,25 @@ const SignUpForm = () => {
     event.preventDefault();
 
     if (password !== confirmPassword) {
-      alert("password do not match");
-
+      alert('passwords do not match');
       return;
     }
 
-    const { user } = createAuthUserWithEmailAndPassword(email, password);
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
 
-    await createUserDocumentFromAuth(user, { displayName });
-
-    resetFormFields();
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Cannot create user, email already in use');
+      } else {
+        console.log('user creation encountered an error', error);
+      }
+    }
   };
 
   const handleChange = (event) => {
@@ -47,47 +55,48 @@ const SignUpForm = () => {
 
     setFormFields({ ...formFields, [name]: value });
   };
+
   return (
-    <div className="sign-up-container">
-      <h2>Don't have an account</h2>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
-      <form onClick={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <FormInput
-          label="Display Name"
-          type="text"
-          onChange={handleChange}
-          name="displayName"
+          label='Display Name'
+          type='text'
           required
+          onChange={handleChange}
+          name='displayName'
           value={displayName}
         />
 
         <FormInput
-          label="Email"
-          type="email"
-          onChange={handleChange}
-          name="email"
+          label='Email'
+          type='email'
           required
+          onChange={handleChange}
+          name='email'
           value={email}
         />
 
         <FormInput
-          label="Password"
-          type="password"
-          onChange={handleChange}
-          name="password"
+          label='Password'
+          type='password'
           required
+          onChange={handleChange}
+          name='password'
           value={password}
         />
 
         <FormInput
-          label="Confirm Password"
-          type="password"
-          onChange={handleChange}
-          name="confirmPassword"
+          label='Confirm Password'
+          type='password'
           required
+          onChange={handleChange}
+          name='confirmPassword'
           value={confirmPassword}
         />
-        <Button type="submit"> Sign Up</Button>
+        <Button type='submit'>Sign Up</Button>
       </form>
     </div>
   );
--- src/util/firebase/firebase.utils.jsx
+++ src/util/firebase/firebase.utils.jsx
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -74,4 +75,11 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
       console.log("user creation encountered an error ", error);
     }
   }
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+  
 };
